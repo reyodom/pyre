13c8c74ecd0204d6740c7c47bc496f5dbdb92097
Sigmund Stal <a@headlessmami.co.cc>
2011-09-22 20:07:35 -0700

    fix reloading

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 # ^ been meaning to put that there for weeks now
-import Core, Parser
-from Utils import Utils
+import Core, Parser, Utils
 import sys, time
 import config
 
 class __init__():
     readbuffer = ""
     Admin = Parser.Admin()
-    Utils = Utils()
-    Parser = Parser.Parser()
+    bUtils = Utils.Utils()
+    bParser = Parser.Parser()
     Core.connect()
     try:
         while True:
@@ -18,7 +17,7 @@ class __init__():
             readbuffer=t.pop()
             for line in t:
                 line=line.split()
-                Utils.displayFormat(line)
+                bUtils.displayFormat(line)
                 # these are more important, so use main to take care of them instead of botparse
                 if(line[1]=="433"):
                     print '--- {0} Nick {1} is already taken!'.format(config.tagW, line[3])
@@ -31,15 +30,15 @@ class __init__():
                 elif(line[0]=="PING"):
                     Core.s.send("PONG {0}\r\n".format(line[1]))
                     print '--- {0} PONGed server. ({1})'.format(config.tagI, line[1].strip(':'))
-                elif(line[1] == "PRIVMSG" and Utils.isChannel(line[2]) and line[3] == ":*rb"):
-                    if Admin.Check(Utils.getHost(line[0])):
+                elif(line[1] == "PRIVMSG" and bUtils.isChannel(line[2]) and line[3] == ":*rb"):
+                    if Admin.Check(bUtils.getHost(line[0])):
                         print '--- {0} Reloading botparse.'.format(config.tagW)
-                        Core.writeSock("PRIVMSG {0} :Okay, reloading botparse.\r\n".format(line[2]))
+                        Core.writeSock("PRIVMSG {0} :Okay, reloading parser and utils.\r\n".format(line[2]))
                         reload(Parser)
                         reload(Utils)
                         Admin = Parser.Admin()
-                        Utils = Utils()
-                        Parser = Parser.Parser()
+                        bUtils = Utils.Utils()
+                        bParser = Parser.Parser()
                     else:
                         Core.writeSock("PRIVMSG {0} :Permission denied.\r\n".format(line[2]))
                 # pass the rest to botparse.
@@ -47,17 +46,17 @@ class __init__():
                     if (line[1] == 'PRIVMSG' or line[1] == 'NOTICE'):
                         try:
                             # format for parser.
-                            Message = Utils.getMessage(line, 0)
+                            Message = bUtils.getMessage(line, 0)
                             MsgSplit = Message.split()
-                            Parser.CommandParser(Utils.getNick(line[0]), Utils.getHost(line[0]), line[1], line[2], Message, MsgSplit)
+                            bParser.CommandParser(bUtils.getNick(line[0]), bUtils.getHost(line[0]), line[1], line[2], Message, MsgSplit)
                         except IndexError:
                             # drop the message if it consists of only whitespace.
                             pass
                     elif line[1] == 'KICK':
-                        Message = Utils.getMessage(line, 1)
-                        Parser.OnKick(Utils.getNick(line[0]), Utils.getHost(line[0]), line[2], line[3], Message)
+                        Message = bUtils.getMessage(line, 1)
+                        bParser.OnKick(bUtils.getNick(line[0]), bUtils.getHost(line[0]), line[2], line[3], Message)
                     else:
-                        Parser.RawParse(line)
+                        bParser.RawParse(line)
                         
     except KeyboardInterrupt:
         # remember to clear the queue before we abruptly quit
